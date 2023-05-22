6711bc6f2a2b27f05db4dfba8370b11f27f1375d
katerin <cuadrakaterin18@gmail.com>
2023-05-21 20:02:48 -0600

    Mi primer commit

---
+++ Activity/Activity_Dato.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AppCurso1._1
+{
+    [Activity(Label = "Activity_Dato")]
+    public class Activity_Dato : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+    }
+}
